afd27117100fe7634b1ebbe408bde4e3fc149e9f
Amos Kyalo <amoskyalo927@gmail.com>
2023-07-04 14:01:46 +0300

    fixed some css bug

--- src/Screens/CheckOut/CheckOut.jsx
+++ src/Screens/CheckOut/CheckOut.jsx
@@ -133,38 +133,44 @@ const CheckOut = ({ route, navigation }) => {
           </View>
 
           <View className="space-y-10 mt-5">
-          {userDetailsInfo.map((info, i) => (
-            <Card heading={info.name} key={i}>
-              <View className="space-y-4 mt-6">
-                {info.details.map((det, i) => (
-                  <FlexDisplay key={i}>
+            {userDetailsInfo.map((info, i) => (
+              <Card heading={info.name} key={i}>
+                <View className="space-y-4 mt-6">
+                  {info.details.map((det, i) => (
                     <FlexDisplay key={i}>
-                      <View className="flex-row gap-5">
-                        <det.icon name={det.iconName} size={20} color="black" />
-                        {typeof det.text === "string" && (
-                          <TouchableOpacity>
-                            <Text>{det.text}</Text>
-                          </TouchableOpacity>
+                      <FlexDisplay key={i}>
+                        <View className="flex-row gap-5">
+                          <det.icon
+                            name={det.iconName}
+                            size={20}
+                            color="black"
+                          />
+                          {typeof det.text === "string" && (
+                            <TouchableOpacity>
+                              <Text>{det.text}</Text>
+                            </TouchableOpacity>
+                          )}
+                          {typeof det.text !== "string" && (
+                            <View className="space-y-3">
+                              {convertObjectKeysToArray(det.text).map(
+                                (el, i) => (
+                                  <Text key={i}>{det.text[`${el}`]}</Text>
+                                )
+                              )}
+                            </View>
+                          )}
+                        </View>
+                        {i === 0 && typeof det.text === "string" && (
+                          <AntDesign name="check" size={22} color="black" />
                         )}
-                        {typeof det.text !== "string" && (
-                          <View className="space-y-3">
-                            {convertObjectKeysToArray(det.text).map((el, i) => (
-                              <Text key={i}>{det.text[`${el}`]}</Text>
-                            ))}
-                          </View>
-                        )}
-                      </View>
-                      {i === 0 && typeof det.text === "string" && (
-                        <AntDesign name="check" size={22} color="black" />
-                      )}
+                      </FlexDisplay>
                     </FlexDisplay>
-                  </FlexDisplay>
-                ))}
-              </View>
-            </Card>
-          ))}
+                  ))}
+                </View>
+              </Card>
+            ))}
           </View>
-          
+
           <View className="flex flex-row justify-between items-center px-2 mt-6">
             <Text className="text-black text-lg font-bold">
               Non-contact-delivery
@@ -173,29 +179,29 @@ const CheckOut = ({ route, navigation }) => {
           </View>
         </View>
 
-        <View className="bg-white rounded-t-[50px] px-8 gap-3">
+        <View className="bg-white rounded-t-[50px] px-6 space-y-3">
           <View className="items-center">
             <View className="border-t-[4px] w-14 border-t-gray-200" />
           </View>
-          <FlexDisplay className="pt-4">
-            <FlexDisplay className="flex-row items-center gap-2 ">
+          <View className="flex flex-row justify-between items-center pt-4">
+            <View className="flex-row items-center gap-2 ">
               <Text className="text-base font-medium">Subtotal</Text>
               <AntDesign name="questioncircle" size={15} color="black" />
-            </FlexDisplay>
+            </View>
             <Text className="text-base font-medium">{item.price}</Text>
-          </FlexDisplay>
+          </View>
           <View className="flex flex-row justify-between items-center pb-4 border-b border-b-gray-200">
             <Text className="text-base font-medium">
               Estimated Delivery & Handling
             </Text>
             <Text className="text-base font-medium">Free</Text>
           </View>
-          <FlexDisplay className="mb-6">
+          <View className="flex flex-row justify-between items-center mb-6">
             <Text className="text-xl font-semibold ">Total</Text>
             <Text className="text-xl text-primaryColor font-semibold">
               {item.price}
             </Text>
-          </FlexDisplay>
+          </View>
           <Pressable className="my-3">
             <Button title="PLACE ORDER" />
           </Pressable>
